9f85d27686e9257d553b85ec7b01a330676f745b
tempnana <94152736+tempnana@users.noreply.github.com>
2022-10-23 12:04:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,8 +13,8 @@ git clone https://github.com/openresty/headers-more-nginx-module
 git clone https://github.com/yaoweibin/nginx_upstream_check_module
 #wget https://ftp.pcre.org/pub/pcre/pcre-8.43.tar.gz && tar xzvf pcre-8.43.tar.gz
 wget https://github.com/tempnana/Nginx/raw/main/pcre-8.45.tar.gz && tar xzvf pcre-8.45.tar.gz
-wget https://www.zlib.net/zlib-1.2.12.tar.gz && tar xzvf zlib-1.2.12.tar.gz
-#wget https://github.com/tempnana/Nginx/raw/main/zlib-1.2.12.tar.gz && tar xzvf zlib-1.2.12.tar.gz
+#wget https://www.zlib.net/zlib-1.2.12.tar.gz && tar xzvf zlib-1.2.12.tar.gz
+wget https://github.com/tempnana/Nginx/raw/main/zlib-1.2.12.tar.gz && tar xzvf zlib-1.2.12.tar.gz
 wget https://nginx.org/download/nginx-1.20.1.tar.gz && tar zxvf nginx-1.20.1.tar.gz
 #wget https://www.openssl.org/source/openssl-1.1.1d.tar.gz && tar xzvf openssl-1.1.1d.tar.gz
 wget https://www.openssl.org/source/openssl-1.1.1o.tar.gz && tar xzvf openssl-1.1.1o.tar.gz

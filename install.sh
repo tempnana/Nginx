3cb3d8b5ccdf529aae90db96970d2b8cdd111f65
tempnana <94152736+tempnana@users.noreply.github.com>
2023-02-12 15:01:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ apt-get install curl wget net-tools iftop build-essential libpcre3 libpcre3-dev 
 adduser --system --home /nonexistent --shell /bin/false --no-create-home --gecos "nginx user" --group --disabled-login --disabled-password nginx
 git clone https://github.com/openresty/sregex
 cd sregex
+sleep 2s
 make && make install
 #get module files and nginx source
 cd
@@ -28,7 +29,7 @@ cd nginx-1.20.1
 #make install
 patch -p1 <  ../nginx_upstream_check_module/check_1.20.1+.patch
 ./configure --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nginx --group=nginx --with-compat --with-file-aio --with-threads --with-http_addition_module --with-http_auth_request_module --with-http_dav_module --with-http_flv_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_mp4_module --with-http_random_index_module --with-http_realip_module --with-http_secure_link_module --with-http_slice_module --with-http_ssl_module --with-http_stub_status_module --with-http_sub_module --with-http_v2_module --with-mail --with-mail_ssl_module --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module --with-pcre=../pcre-8.45 --with-pcre-jit --with-zlib=../zlib-1.2.12 --with-openssl=../openssl-1.1.1o --with-openssl-opt=no-nextprotoneg --with-debug --add-module=../ngx_cache_purge --add-module=../ngx_http_substitutions_filter_module --add-module=../nginx_upstream_check_module --add-module=../headers-more-nginx-module --add-module=../replace-filter-nginx-module
-sleep 1
+sleep 2s
 make && make install
 cd
 #ln
